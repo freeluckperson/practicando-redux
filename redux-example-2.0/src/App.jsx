a02cb2961dbecc2dd0b63542414670cd3de434be
freeluckperson <gestionecesaria@gmail.com>
2023-11-07 10:10:41 -0400

    primer commit

---
+++ redux-example-2.0/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <h2>React + RTK</h2>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ redux-example-2.0/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-secondary">
+      <div className="container">
+        <h2>Soy una navbar</h2>
+        <a className="navbar-brand" href="/">
+          React + RTK
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
